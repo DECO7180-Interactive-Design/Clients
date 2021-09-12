bae0b9c4f732459af79d5f5aaaf08c2027311f59
Allen Liu <allen.liu13@outlook.com>
2021-09-12 20:37:46 +1000

    Add simple queries for the featuresLayer instance.

--- js/route.js
+++ js/route.js
@@ -1,11 +1,30 @@
-let map = L.map('map').setView([-27.495432, 153.012024], 11.5);
+let map = L.map('map').setView([-27.495432, 153.012024], 15);
 
 L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
     apikey: "AAPKb74ce1d9657b4e7a937e5d491f507f80I6a303ICiJJ0cg5QgZhry93CHhjFPZOFp6LzeYpZ9JGcBgN1mdLirFVut47IjMZ-"
 }).addTo(map);
 
 let routes = L.esri.featureLayer({
-    url: 'https://services2.arcgis.com/dEKgZETqwmDAh1rP/ArcGIS/rest/services/Bicycle_network_overlay/FeatureServer/0'
+    url: 'https://services2.arcgis.com/dEKgZETqwmDAh1rP/ArcGIS/rest/services/Bicycle_network_overlay/FeatureServer/0',
+    where: "DESCRIPTION = 'Primary cycle route'"
 });
 
-routes.addTo(map);
+console.log(routes);
+
+// Add a bounds aroud uq.
+let southWest = L.latLng(-27.4943556, 153.0038058);
+let northEast = L.latLng(-27.5027481, 153.019045);
+let bounds = L.latLngBounds(southWest, northEast);
+
+// Query for the routes within the bounds.
+routes.query()
+    .within(bounds)
+    .run(function (error, featureCollection) {
+        console.log(featureCollection);
+
+        L.geoJSON(featureCollection).addTo(map);
+    });
+
+// Add routes to basic map.
+// routes.addTo(map);
+// routes.setWhere("DESCRIPTION = 'Secondary cycle route'");

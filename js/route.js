4db21756974bef5fc09795347b083a801946bdd2
Allen Liu <allen.liu13@outlook.com>
2021-09-12 19:02:44 +1000

    Assign the featureLayer to the routes variable.

--- js/route.js
+++ js/route.js
@@ -1,9 +1,11 @@
-var map = L.map('map').setView([-27.495432, 153.012024], 12);
+let map = L.map('map').setView([-27.495432, 153.012024], 11.5);
 
 L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
-    apikey: "AAPKb74ce1d9657b4e7a937e5d491f507f80I6a303ICiJJ0cg5QgZhry93CHhjFPZOFp6LzeYpZ9JGcBgN1mdLirFVut47IjMZ-" // Replace with your API key - https://developers.arcgis.com
+    apikey: "AAPKb74ce1d9657b4e7a937e5d491f507f80I6a303ICiJJ0cg5QgZhry93CHhjFPZOFp6LzeYpZ9JGcBgN1mdLirFVut47IjMZ-"
 }).addTo(map);
 
-L.esri.featureLayer({
+let routes = L.esri.featureLayer({
     url: 'https://services2.arcgis.com/dEKgZETqwmDAh1rP/ArcGIS/rest/services/Bicycle_network_overlay/FeatureServer/0'
-}).addTo(map);
+});
+
+routes.addTo(map);

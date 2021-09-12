0afa5afb7d13899a08fb0c22c5df310984b85d17
Allen Liu <allen.liu13@outlook.com>
2021-09-12 20:40:46 +1000

    Replace the arcgid api map the the esri's basic map.

--- js/route.js
+++ js/route.js
@@ -1,8 +1,13 @@
 let map = L.map('map').setView([-27.495432, 153.012024], 15);
 
-L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
-    apikey: "AAPKb74ce1d9657b4e7a937e5d491f507f80I6a303ICiJJ0cg5QgZhry93CHhjFPZOFp6LzeYpZ9JGcBgN1mdLirFVut47IjMZ-"
-}).addTo(map);
+// FIXME the arcgis' map has some problems.
+
+// L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
+//     apikey: "AAPKb74ce1d9657b4e7a937e5d491f507f80I6a303ICiJJ0cg5QgZhry93CHhjFPZOFp6LzeYpZ9JGcBgN1mdLirFVut47IjMZ-"
+// }).addTo(map);
+
+// Using the basic map to replace the api map.
+L.esri.basemapLayer('Topographic').addTo(map);
 
 let routes = L.esri.featureLayer({
     url: 'https://services2.arcgis.com/dEKgZETqwmDAh1rP/ArcGIS/rest/services/Bicycle_network_overlay/FeatureServer/0',

ed9c5943545aec1a9a8ec8787b2adabc3cd5d9b0
RuiqiJia <j1298878163@gmail.com>
2021-09-16 01:11:30 +1000

    modify touring

--- js/touring.js
+++ js/touring.js
@@ -1,3 +1,4 @@
+const whole = document.querySelector('.container')
 const music = document.querySelector('.music')
 const playBtn =document.querySelector('#play')
 const prevBtn =document.querySelector('#prev')
@@ -6,7 +7,11 @@ const title = document.querySelector('#title')
 const audio = document.querySelector('#audio')
 const songs = ['Levitating', 'I Feel It Coming', 'Blinding Lights']
 const rest = document.querySelector('#pauseRiding')
-
+const hideBtn = document.querySelector('.hideAll')
+const map = document.querySelector('.mapbox')
+const controlBtn = document.querySelector('.controlBtn')
+const dropdown = document.querySelector(".dropDown")
+whole.classList.add('show')
 let songIndex = 0
 
 loadSong(songs[songIndex])
@@ -16,6 +21,7 @@ function loadSong(song) {
     audio.src = `assets/${song}.mp3`
 }
 
+// play and pause songs
 function playSong() {
     music.classList.add('play')
     playBtn.querySelector('i.fas').classList.remove('fa-play')
@@ -30,6 +36,7 @@ function pauseSong() {
     audio.pause()
 }
 
+//take a rest or keep riding
 function takeRest() {
     rest.classList.remove('play')
     rest.querySelector('i.fas').classList.remove('fa-play')
@@ -42,6 +49,7 @@ function resumeRiding() {
     rest.querySelector('i.fas').classList.remove('fa-pause')
 }
 
+// next and last song
 function prevSong() {
     songIndex--
     if(songIndex < 0) {
@@ -60,6 +68,37 @@ function nextSong() {
 
     loadSong(songs[songIndex])
     playSong()
+}
+
+// pop up dialogue
+function confirmAction() {
+    var change = false;
+    let confirmAction = confirm("Are you sure to end this tour?")
+    if(confirmAction==true) {
+        // return true;
+        window.location.replace("new target URL");
+    } else {return false;}
+}
+
+// hide and show all buttons
+function hideAll() {
+    whole.classList.remove('show')
+    hideBtn.querySelector('i.fas').classList.remove('fa-eye-slash')
+    hideBtn.querySelector('i.fas').classList.add('fa-eye')
+    music.style.display = "none";
+    map.style.display = "none";
+    controlBtn.style.display = "none";
+    dropdown.style.display = "none";
+}
+
+function showAll() {
+    whole.classList.add('show')
+    hideBtn.querySelector('i.fas').classList.remove('fa-eye')
+    hideBtn.querySelector('i.fas').classList.add('fa-eye-slash')
+    music.style.display = "block";
+    map.style.display = "block";
+    controlBtn.style.display = "block";
+    dropdown.style.display = "block";
 }
 
 playBtn.addEventListener('click', () => {
@@ -82,5 +121,50 @@ rest.addEventListener('click', () => {
     }
 })
 
+hideBtn.addEventListener('click', () => {
+    const isShowing = whole.classList.contains('show')
+    
+    if(isShowing) {
+        hideAll()
+    } else {
+        showAll()
+    }
+})
+
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+
+// map popup
+var modal = document.getElementById("mapModal");
+
+var img = document.getElementById("mapImg");
+var modalImg = document.getElementById("img1");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// dropdown
+function showMenu() {
+    document.getElementById("options").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('#dbutton')) {
+      var dropdowns = document.getElementsByClassName("dropdown-option");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }

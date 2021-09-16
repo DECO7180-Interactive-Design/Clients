fb935ed6b9dd64541de1594efbfd61acf5ac6ac5
Allen Liu <allen.liu13@outlook.com>
2021-09-16 22:15:36 +1000

    Add condition control for exerciseLevel.

--- js/route.js
+++ js/route.js
@@ -14,7 +14,7 @@ const routes = L.esri.featureLayer({
     // where: "DESCRIPTION = 'Primary cycle route'"
 }).addTo(map);
 
-// console.log(routes);
+console.log(routes);
 
 // Set the style (colour) of different route type.
 let primaryStyle = {
@@ -29,31 +29,31 @@ let secondaryStyle = {
 let southWest = L.latLng(-27.5014174, 152.9891076);
 let northEast = L.latLng(-27.4776612, 153.0417289);
 let bounds = L.latLngBounds(southWest, northEast);
-map.fitBounds(bounds);
+// map.fitBounds(bounds);
 
 // Query for primary clcle routes
-routes.query()
-    // .within(bounds)
-    .where("DESCRIPTION = 'Primary cycle route'")
-    .run(function (error, primaryRoutes) {
-        // console.log(primaryRoutes);
+// routes.query()
+//     // .within(bounds)
+//     .where("DESCRIPTION = 'Primary cycle route'")
+//     .run(function (error, primaryRoutes) {
+//         // console.log(primaryRoutes);
 
-        L.geoJSON(primaryRoutes, {
-            style: primaryStyle
-        }).addTo(map);
-    });
+//         L.geoJSON(primaryRoutes, {
+//             style: primaryStyle
+//         }).addTo(map);
+//     });
 
-// Query for local cycle routes.
-routes.query()
-    // .within(bounds)
-    .where("DESCRIPTION = 'Local cycle route'")
-    .run(function (error, secondaryRoutes) {
-        // console.log(secondaryRoutes);
+// // Query for local cycle routes.
+// routes.query()
+//     // .within(bounds)
+//     .where("DESCRIPTION = 'Local cycle route'")
+//     .run(function (error, secondaryRoutes) {
+//         // console.log(secondaryRoutes);
 
-        L.geoJSON(secondaryRoutes, {
-            style: secondaryStyle
-        }).addTo(map);
-    });
+//         L.geoJSON(secondaryRoutes, {
+//             style: secondaryStyle
+//         }).addTo(map);
+//     });
 
 
 // Add routes to basic map.
@@ -71,5 +71,17 @@ routes.query()
 let filterButton = document.getElementById("filterButton");
 
 filterButton.addEventListener("click", function() {
-    routes.setWhere("DESCRIPTION = 'Primary cycle route'");
+    let exerciseLevel = document.getElementById("filter").elements.namedItem("exerciseLevel").value;
+    let ridingTime = document.getElementById("filter").elements.namedItem("ridingTime").value;
+
+    if (exerciseLevel == "easy") {
+        console.log(exerciseLevel, ridingTime);
+        routes.setWhere('Shape__Length < 2000');
+    } else if (exerciseLevel == "balanced") {
+        console.log(exerciseLevel, ridingTime);
+        routes.setWhere('Shape__Length >= 2000 and Shape__Length < 5000');
+    } else if (exerciseLevel == "hard") {
+        console.log(exerciseLevel, ridingTime);
+        routes.setWhere('Shape__Length >=5000');
+    }
 });

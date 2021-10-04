2dd797a9eba969bc13ae9dcb4e303726842ae9d0
Tony <anicedaytony@outlook.com>
2021-10-04 16:45:45 +1000

    competition

---
+++ js/competition.js
@@ -0,0 +1,10 @@
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+
+button1.addEventListener("click", (event) => {
+  alert("No competition today!");
+});
+
+button2.addEventListener("click", (event) => {
+  alert("No competition today!");
+});

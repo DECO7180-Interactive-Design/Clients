d9b378e1e0b3ba3f707ed189c20ac785c7e95dd5
ZipuTang <88757819+ZipuTang@users.noreply.github.com>
2021-10-18 12:29:00 +1000

    account
    create account pop up , this pop up has an independent css to avoid bug

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(".unselected").click(function(){
+    $(".fixed").css("display","block")
+    $(".con").css("display","block");
+})
+$(".fi").click(function(){
+    $(".con").css("display","none");
+     $(".fixed").css("display","none")
+})
